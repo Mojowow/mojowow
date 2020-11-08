ed4ed628953868ff70ef1b90508238a9abba49b3
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2020-11-08 21:41:09 +0100

    - run scripts - updated gitignore

---
+++ run-tbc/mangosd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+EXECUTABLE=$( tail -n 1 ${DIR}/mangosd-latest )
+
+EXECUTABLE_DIR=$(dirname "${EXECUTABLE}")
+
+cd $EXECUTABLE_DIR;
+./mangosd
