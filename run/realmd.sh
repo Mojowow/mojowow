5e748130bf0fcd1cf9a68d59a69fca7e705bee3d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2020-11-09 01:12:44 +0100

    moved run folder

---
+++ run/realmd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+EXECUTABLE=$( tail -n 1 ${DIR}/../log/realmd-latest )
+
+EXECUTABLE_DIR=$(dirname "${EXECUTABLE}")
+
+cd $EXECUTABLE_DIR;
+./realmd
